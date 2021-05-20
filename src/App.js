84ef09abd5ae2ab15471c95f6fc2880ae32d4539
Frank Downing <fdowning@Franks-MacBook-Air.local>
2021-05-20 15:15:14 -0400

    Update paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ARK to the moon!
         </p>
         <a
           className="App-link"
